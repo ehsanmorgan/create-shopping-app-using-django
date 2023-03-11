3919bdb768471c396d26a0c7c7e5439c2427848b
ehsanmorgan <ehsanmorgan1994@gmail.com>
2023-03-11 18:49:38 +0100

    add review count

--- shop/serializer.py
+++ shop/serializer.py
@@ -1,12 +1,24 @@
 from rest_framework import serializers
-from .models import Electronic
+from .models import Electronic,Reviews
 
 
 
 class electronicListSerialize(serializers.ModelSerializer):
+    price_tex=serializers.SerializerMethodField('mynew_price')
+    review_count=serializers.SerializerMethodField()
+    
     class Meta:
         model=Electronic
         fields='__all__'
+        
+    def get_review_count(self,Electronic):
+        rview=Electronic.review_electronic.all().count()
+        return rview
+        
+        
+    def mynew_price(self,Electronic):
+        return Electronic.price*1.2
+            
 
 
 class electronicDetailSerialize(serializers.ModelSerializer):
