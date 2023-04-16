a55e73d7f712a2904ea3e30db41d0fff88c15cb8
ehsanmorgan <ehsanmorgan1994@gmail.com>
2023-04-16 20:03:27 +0200

    -

--- center/settings.py
+++ center/settings.py
@@ -158,3 +158,13 @@ GRAPH_MODELS = {
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
+
+
+LOGIN_REDIRECT_URL = '/'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+AUTHENTICATION_BACKENDS=['accounts.backends.EmailBackend']
